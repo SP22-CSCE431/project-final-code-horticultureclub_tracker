a8747375633dacb9a68baece82b2ba54693a1fc0
dylanharden3 <dylanharden3@tamu.edu>
2022-02-12 12:25:44 -0600

    added procfile and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :points
   resources :members
   resources :events
+  root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
