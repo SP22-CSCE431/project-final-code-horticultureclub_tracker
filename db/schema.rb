2ecc37c1cfe32b8df06257723a5e3d0670f5cf0b
dylanharden3 <dylanharden3@tamu.edu>
2022-02-15 09:38:11 -0600

    removed plantid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_152310) do
+ActiveRecord::Schema.define(version: 2022_02_14_224217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
