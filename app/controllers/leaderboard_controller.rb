de2a2386bef777835ee8567daddf0f81afc9a176
jhassmann <15932+jhassmann@users.noreply.github.tamu.edu>
2022-02-22 09:13:03 -0600

    Leaderboard is sorted by points totals

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,8 +3,9 @@ class LeaderboardController < ApplicationController
 
     @members = Point
     .joins("INNER JOIN members ON members.id=points.member_id")
-    .group('members.name, points.member_id')
+    .group('members.name, points.member_id, points.points')
     .select('members.name, points.member_id as id, sum(points.points) as points')
+    .order('points DESC')    
     
   end
 end
