94150adff3a19d7c3f67d574a9d97049cabdde03
dylanharden3 <dylanharden3@tamu.edu>
2022-03-01 11:38:10 -0600

    fixed merge syntax error

--- config/boot.rb
+++ config/boot.rb
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup'
 require 'bootsnap/setup'
-=======
-# frozen_string_literal: true
-
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
-require 'bundler/setup'
-require 'bootsnap/setup'
->>>>>>> test
