d236365cac2835942b0e8f90903b2547de9edd4e
dylanharden3 <dylanharden3@tamu.edu>
2022-02-28 21:17:43 -0600

    fixed some rubocop offenses and updated styles for admin pages

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,11 +3,10 @@
 class LeaderboardController < ApplicationController
   def index
     @members = Point
-    .joins("INNER JOIN members ON members.id=points.member_id")
-    .joins("INNER JOIN events ON events.id=points.event_id")
-    .group('members.name, points.member_id, events.points')
-    .select('members.name, points.member_id as id, sum(events.points) as points')
-    .order('points DESC')    
-    
+               .joins('INNER JOIN members ON members.id=points.member_id')
+               .joins('INNER JOIN events ON events.id=points.event_id')
+               .group('members.name, points.member_id, events.points')
+               .select('members.name, points.member_id as id, sum(events.points) as points')
+               .order('points DESC')
   end
 end
