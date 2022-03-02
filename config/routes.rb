0f847f58099747aa4d84454ed3f159cbc80d4c96
Flappjack1 <69977572+Flappjack1@users.noreply.github.com>
2022-03-02 11:43:36 -0600

    reimplemented base oauth due to merge issues

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   # resources :admins
 
   root 'dashboard#index'
-  # root to: 'dashboards#show'
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
 
   get 'leaderboard', to: 'leaderboard#index'
   get 'dashboard', to: 'dashboard#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_25_144525) do
+ActiveRecord::Schema.define(version: 2022_03_02_065803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "event_type"
