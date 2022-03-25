da79a51069c000afc36fbda310494cbb28732da7
dylanharden3 <dylanharden3@tamu.edu>
2022-03-25 16:22:22 -0500

    removed imagefile tests, views, controller and model

--- app/views/imagefiles/index.json.jbuilder
+++
@@ -1,3 +0,0 @@
-# frozen_string_literal: true
-
-json.array!(@imagefiles, partial: 'imagefiles/imagefile', as: :imagefile)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :imagefiles
+  
   get 'admins/index'
   resources :eligible_admins
   resources :positions
