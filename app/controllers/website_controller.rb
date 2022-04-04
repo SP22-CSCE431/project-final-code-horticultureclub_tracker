da4bb3467a32bfd795668ec1c2ed484b7fc169c7
dylanharden3 <dylanharden3@tamu.edu>
2022-04-04 16:23:04 -0500

    added positions to website

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class WebsiteController < ApplicationController
-  def index; end
+  def index
+    @positions = Position.all
+  end
 end
--- app/serializers/position_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class PositionSerializer < ActiveModel::Serializer
-  attributes :id, :position_name, :member_id
-end
