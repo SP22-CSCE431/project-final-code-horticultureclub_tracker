7e1b476e06af3bb48580f75f735d0f7bcb53b7a6
dylanharden3 <dylanharden3@tamu.edu>
2022-04-05 14:46:39 -0500

    fixed typo in website controller

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class WebsiteController < ApplicationController
-  def index; end
-
+  def index
+    @positions = Position.all
+  end
   def plantsale
     @annuals = Plant.all.where("category = 'Annuals'")
     @perennials = Plant.all.where("category = 'Perennials'")
@@ -12,7 +13,5 @@ class WebsiteController < ApplicationController
     @succulents = Plant.all.where("category = 'Succulents'")
     @fruits = Plant.all.where("category = 'Fruits'")
     @vegetables = Plant.all.where("category = 'Vegetables'"
-  def index
-    @positions = Position.all
-  end
+
 end
