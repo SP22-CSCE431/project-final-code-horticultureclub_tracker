ab71aea366b15e5f502b6e20002022025d31ef83
dylanharden3 <dylanharden3@tamu.edu>
2022-04-10 17:31:59 -0500

    fixed rubocop bug

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class HelpController < ApplicationController
-	before_action :authenticate_admin!
-	def help; end
+  before_action :authenticate_admin!
+  def help; end
 end
